a59ee4b696380dd81b8bacd7fa094fa2ecb21e83
Alec Gunny <alec.gunny@gmail.com>
2021-05-18 13:25:55 -0700

    cleaning up logging in wait_for

--- cloud_utils/utils.py
+++ cloud_utils/utils.py
@@ -37,7 +37,7 @@ def wait_for(
 
     i, exit_pad = 0, 0
     value = None
-    logger = logging.info
+    level = logging.INFO
     try:
         while t.is_alive():
             value = check_q()
@@ -56,12 +56,10 @@ def wait_for(
             value = check_q()
     except Exception:
         exit_msg = "Encountered error in callback"
-        logger = logging.error
+        level = logging.ERROR
         raise
     finally:
+        print(" " * exit_pad, end="\r", flush=True)
         if exit_msg is not None:
-            exit_msg = exit_msg + " " * (exit_pad - len(exit_msg))
-            logger(exit_msg)
-        else:
-            print(" " * exit_pad, end="\r", flush=True)
+            logging.log(exit_msg, level=level)
     return value
